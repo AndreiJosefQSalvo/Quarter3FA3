7754f9f27b27d498b009d789c9845a5b55be62fd
AndreiJosefQSalvo <andreijosef.salvo@smc.pshs.edu.ph>
2025-02-24 20:25:18 +0800

    Add files via upload

---
+++ rand.js
@@ -0,0 +1,41 @@
+function numbers() {
+	var num1 = Math.random() * 20;
+	var num2 = Math.random() * 20;
+	var num3 = Math.random() * 20;
+
+	num1 = Math.ceil(num1);
+	num2 = Math.ceil(num2);
+	num3 = Math.ceil(num3);
+
+	document.getElementById('num1').innerHTML = num1;
+	document.getElementById('num2').innerHTML = num2;
+	document.getElementById('num3').innerHTML = num3;
+
+var high = Math.max(num1, num2, num3);
+var highestnums = [];
+var highestprods = [];
+
+if(num1 == high) highestnums.push(num1);
+if(num2 == high) highestnums.push(num2);
+if(num3 == high) highestnums.push(num3);
+
+if(num1 == high) highestprods.push("Chips");
+if(num2 == high) highestprods.push("Bottled Water");
+if(num3 == high) highestprods.push("Instant Noodles");
+
+document.getElementById('highnum').innerHTML = highestnums;
+document.getElementById('product').innerHTML = highestprods;
+
+var al = "abcdefghijklmnopqrstuvwxyz";
+var letter = al.charAt(num1 - 1);
+
+document.getElementById('lett').innerHTML = letter;
+
+var mins = num2*num3;
+var hr = mins/60;
+var min = mins%60;
+
+document.getElementById('hours').innerHTML = parseInt(hr);
+document.getElementById('minutes').innerHTML = parseInt(min);
+
+}
